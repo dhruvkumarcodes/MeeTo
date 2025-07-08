fe2d9c550c37c23ef59b287d40bba942cd407b3a
dhruvkumarcodes <kdhruv7505@gmail.com>
2025-07-08 20:55:32 +0530

    Completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,10 +5,15 @@ import userRoutes from "./routes/userroute.js"
 import chatRoutes from "./routes/chatroute.js"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config();
 
 const app = express()
 const PORT = process.env.PORT
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
